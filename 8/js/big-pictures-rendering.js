23b1722bb5b6272788a2288c232faadc3a476669
Keks <keks@htmlacademy.ru>
2023-03-27 09:18:12 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/big-pictures-rendering.js
@@ -0,0 +1,80 @@
+import { isEscapeKey } from './util.js';
+import { dataForModal } from './data.js';
+
+const modalElement = document.querySelector('.big-picture');
+const socialLikesCount = modalElement.querySelector('.likes-count');
+const socialCommentsCount = modalElement.querySelector('.social__comment-count');
+const closeModalElement = modalElement.querySelector('.big-picture__cancel');
+const socialCommentConteiner = modalElement.querySelector('.social__comments');
+const socialCommentTemplate = modalElement.querySelector('.social__comment');
+const commentsLoaderButton = modalElement.querySelector('.social__comments-loader');
+const body = document.querySelector('body');
+dataForModal.commentsArray = [];
+dataForModal.commentUnlock = 0;
+dataForModal.COMMENT_BLOCK = 5;
+
+const renderComment = (comment) => {
+  const template = socialCommentTemplate;
+  const commentsListFragment = document.createDocumentFragment();
+  const newFragment = template.cloneNode(true);
+  newFragment.querySelector('.social__picture').src = comment.avatar;
+  newFragment.querySelector('.social__picture').alt = comment.name;
+  newFragment.querySelector('.social__text').textContent = comment.message;
+  commentsListFragment.append(newFragment);
+  return commentsListFragment;
+};
+
+const loaderComments = (comments) => {
+  const fragment = document.createDocumentFragment();
+  dataForModal.commentUnlock += dataForModal.COMMENT_BLOCK;
+  if (dataForModal.commentUnlock >= comments.length) {
+    commentsLoaderButton.classList.toggle('hidden');
+    dataForModal.commentUnlock = comments.length;
+  }
+  for (let i = 0; i < dataForModal.commentUnlock; i++) {
+    const commentElement = renderComment(comments[i]);
+    fragment.append(commentElement);
+  }
+  socialCommentConteiner.innerHTML = '';
+  socialCommentConteiner.append(fragment);
+  socialCommentsCount.innerHTML = `${dataForModal.commentUnlock} из <span class="comments-count">${comments.length}</span> комментариев</div>`;
+};
+
+const renderBigPicture = (picture) => {
+  body.classList.add('modal-open');
+  modalElement.classList.remove('hidden');
+  modalElement.querySelector('.big-picture__img img').src = picture.url;
+  modalElement.querySelector('.social__caption').textContent = picture.description;
+  socialLikesCount.textContent = picture.likes;
+  dataForModal.commentsArray = Array.from(picture.comments);
+  dataForModal.commentUnlock = 0;
+  loaderComments(dataForModal.commentsArray);
+};
+
+const onModalEscKeydown = (evt) => {
+  if(isEscapeKey(evt)) {
+    evt.preventDefault();
+    closeModal();
+  }
+};
+
+function loadComments () {
+  loaderComments(dataForModal.commentsArray);
+}
+
+function closeModal () {
+  modalElement.classList.add('hidden');
+  body.classList.remove('modal-open');
+}
+
+closeModalElement.addEventListener('click', () => {
+  closeModal();
+});
+
+commentsLoaderButton.addEventListener('click', () => {
+  loadComments();
+});
+
+document.addEventListener('keydown', onModalEscKeydown);
+
+export {renderBigPicture};
---
+++ 8/js/data.js
@@ -0,0 +1,77 @@
+import {getRandomInteger, getRandomArrayElement, createIdGenerator} from './util.js';
+
+// Основные параметры и массивы
+const PICTURE_COUNT = 25;
+const AVATAR_COUNT = 6;
+const LIKES_COUNT = 200;
+const COMMENT_COUNT = 20;
+const COMMENT_LINES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+const NAMES = ['Игорь', 'Денис', 'Анастасия', 'Валлера', 'Елена', 'Кирилл','Булат',];
+const DESCRIPTIONS = [
+  'Вид набережной с отелем.',
+  'Табличка с описанием направления к пляжу',
+  'Вид на океан',
+  'Mama Mia!',
+  'Вкусняшка',
+  'Мафынка',
+  'Ягодки',
+  'Чаёк',
+  'Пляж',
+  'Подставка для обуви',
+  'Путь на пляж',
+  'Красиво ... :)',
+  'Великолепное блюдо',
+  'Ха-ха, смешно :)',
+  'Прикольно',
+  'Летим',
+  'Хорошо',
+  'Мафынка',
+  'Тапки светодиодки',
+  'Отель',
+  'Ненавижу спаржу',
+  'Закат',
+  'Крабик',
+  'Концерт',
+  'Опасность'
+];
+
+// Параметры для отоборожения модалки
+const dataForModal = {
+  commentsArray: [],
+  commentUnlock: 0,
+  COMMENT_BLOCK: 5
+};
+
+// Функции получения рандомных картинок и комментариев.
+const generateCommentId = createIdGenerator();
+
+const getRandomCommentLines = () => Array.from({length: getRandomInteger(1, 2)}, () => getRandomArrayElement(COMMENT_LINES)).join(' ');
+
+const createComments = () => ({
+  id: generateCommentId(),
+  avatar: `img/avatar-${getRandomInteger(1, AVATAR_COUNT)}.svg`,
+  message: getRandomCommentLines(),
+  name: getRandomArrayElement(NAMES)
+});
+
+const createPictures = (index) => ({
+  id: index,
+  url: `photos/${index}.jpg`,
+  description: getRandomArrayElement(DESCRIPTIONS),
+  likes: getRandomInteger(15, LIKES_COUNT),
+  comments: Array.from({length: getRandomInteger(0, COMMENT_COUNT)}, createComments)
+});
+
+const getPictures = () =>
+  Array.from({length: PICTURE_COUNT}, (_, pictureIndex) =>
+    createPictures(pictureIndex + 1));
+
+export {getPictures, dataForModal};
+
