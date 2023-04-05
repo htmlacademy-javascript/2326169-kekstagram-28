1d38bf0f0801b42334bd3b57313ac8a1da56fe62
Яганов Денис <yaganovdo@gmail.com>
2023-04-05 11:33:43 +0300

    Подключение модуля effects

--- js/main.js
+++ js/main.js
@@ -2,5 +2,6 @@ import {getPictures} from './data.js';
 import {renderGallery} from './gallery.js';
 import {} from './form.js';
 import {} from './scale.js';
+import {} from './effects.js';
 
 renderGallery(getPictures());
